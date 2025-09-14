1cb046b20a8b0401840e641feaacf16e37b20b3c
Lana <kainingyang22@gmail.com>
2025-09-14 17:19:24 +0800

    修改手機樣式

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", usersRouter);
 app.use(
-  "/chatRoom",
+  "/chat",
   passport.authenticate("jwt", { session: false }),
   chatRoomRoute
 );
