cef71aae49e10061fc11d21d2fb17c6f32f31710
Lana <kainingyang22@gmail.com>
2025-09-21 16:59:28 +0800

    修正chat

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", usersRouter);
-app.use(express.static(path.join(__dirname, "../vue/dist")));
+app.use("/chat", express.static(path.join(__dirname, "../vue/dist")));
 app.use(
   "/api/chat",
   passport.authenticate("jwt", { session: false }),
